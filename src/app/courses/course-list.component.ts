74250673a1c68e9be56c12ba7987b3308e6ac60e
Diego <diegorf55@gmail.com>
2021-10-24 20:55:58 -0300

    create course service

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
+import { CouseService } from "./course.service";
 
 @Component({
     selector: 'app-course-list',
@@ -11,32 +12,10 @@ export class CourseListComponent implements OnInit{
 
 course: Course[] = [];
 
+constructor(private courseService: CouseService){}
+
 ngOnInit(): void{
-this.course = [
-    {
-        id: 1,
-        name: 'Compartilhando Conhecimento',
-        imageUrl: '/assets/images/4.png',
-        price: 99.99,
-        code: 'abc',
-        duration: 120,
-        rating: 5,
-        releaseDate:'November, 2, 2020',
-        option: ''
-     
-    },
-    {
-        id: 2,
-        name: 'Git',
-        imageUrl: '/assets/images/logo-git.png',
-        price: 98,
-        code: 'EFG',
-        duration: 120,
-        rating: 4.5,
-        releaseDate:'December, 2, 2019',
-        option: ''
-    }
-]
+this.course = this.courseService.retrieveAll();
 
 }
 
